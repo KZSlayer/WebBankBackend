482a806fbd32ca943c3415b15b3df423210839fe
KZSlayer <nokitakaze@gmail.com>
2024-11-19 19:45:43 +0300

    Добавлена модель User для представления таблицы пользователей

---
+++ Identity/Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Identity.Models
+{
+    [Table("users")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        [StringLength(12)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
