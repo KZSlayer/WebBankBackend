a8163d86502ed1ada974204fef061e54793539d6
KZSlayer <nokitakaze@gmail.com>
2024-11-19 21:26:47 +0300

    Добавлена модель RefreshToken для представления таблицы токенов

---
+++ Identity/Models/RefreshToken.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Identity.Models
+{
+    [Table("refresh_tokens")]
+    public class RefreshToken
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Token { get; set; }
+
+        [Required]
+        public DateTime ExpiryDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? RevokedAt { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
