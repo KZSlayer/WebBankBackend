409ee9f9c7768d32c11618db828631983fdffafb
KZSlayer <nokitakaze@gmail.com>
2024-11-20 21:38:25 +0300

    Добавлен и настроен IdentityDbContext для PostgreSQL

---
+++ Identity/Models/IdentityDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Identity.Models
+{
+    public class IdentityDbContext : DbContext
+    {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options):base(options) { }
+        public DbSet<User> users { get; set; }
+        public DbSet<RefreshToken> refresh_tokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.PhoneNumber)
+                .IsUnique();
+        }
+    }
+}
--- Identity/Program.cs
+++ Identity/Program.cs
@@ -1,4 +1,18 @@
+using Identity.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables();
+
+string connectionString =
+    $"Host={builder.Configuration["IDENTITY_DB_HOST"]};" +
+    $"Port={builder.Configuration["IDENTITY_DB_PORT"]}" +
+    $"Database={builder.Configuration["IDENTITY_DB_NAME"]}" +
+    $"Username={builder.Configuration["IDENTITY_DB_USER"]}" +
+    $"Password={builder.Configuration["IDENTITY_DB_PASSWORD"]}";
+
+builder.Services.AddDbContext<IdentityDbContext>(options => options.UseNpgsql(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
