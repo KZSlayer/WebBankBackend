9400f2f85f4abbfc85636caf0ecb554d29b6acc0
KZSlayer <nokitakaze@gmail.com>
2024-11-24 00:14:51 +0300

    Добавил папку с нереализованным микросервисом Transaction

---
+++ Transaction/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseAuthentication();
+app.MapControllers();
+
+app.Run();
